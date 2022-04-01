a73e683b637eafa8e0154c86cd678e4ca660234e
feverus <98666447+feverus@users.noreply.github.com>
2022-04-01 22:21:44 +0300

    25

--- src/Main.js
+++ src/Main.js
@@ -241,7 +241,7 @@ class Main extends React.Component {
 				let result = JSON.parse(xhr.response);
 				console.log('version: local ' + this.state.version + ', server: ' + result.version);
 				if ((result.a !== '0') & (result.a !== '404')) {
-					if ((+0+this.state.version) <= (+0+result.version)) {
+					if ((+0+this.state.version) < (+0+result.version)) {
 						console.log('синхронизация');
 						updated = true;
 						this.playersCopy = JSON.parse(JSON.stringify(result.players));
@@ -381,7 +381,7 @@ class Main extends React.Component {
 				console.log('STOP update');
 				this.setState({update: "off"});
 			} else {
-				this.nextUpdate = setTimeout(this.LoadJSONrepeat, 500);
+				this.nextUpdate = setTimeout(this.LoadJSONrepeat, 200);
 				console.log('START update');
 				this.setState({update: "on"});
 			}			
